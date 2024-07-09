64648c5114a80b8c24a9fb6e08e0973532db3bb7
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-07-09 12:46:40 +0530

    Improved Layout Issues

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PageExpenses from "./pages/expenses";
+import PageExpenses from "./pages/transactions";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>
-);
-
-function AppRouter() {
-  return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PageExpenses />} />
       </Routes>
     </BrowserRouter>
-  );
-}
+  </React.StrictMode>
+);
